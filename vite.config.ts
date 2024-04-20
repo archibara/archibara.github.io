ddb94568ba98483ab226354eca327e0f6b2cb694
Artur Perepelytsia <artur.perepelytsia.m@gmail.com>
2024-04-20 17:27:24 +0300

    vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        'theme_color': '#f69435',
+        'background_color': '#f69435',
+        'scope': '/',
+        'start_url': '/',
+        'icons': [
+          {
+            'src': '/icon-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png'
+          },
+          {
+            'src': '/icon-256x256.png',
+            'sizes': '256x256',
+            'type': 'image/png'
+          },
+          {
+            'src': '/icon-384x384.png',
+            'sizes': '384x384',
+            'type': 'image/png'
+          },
+          {
+            'src': '/icon-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png'
+          }
+        ]
+      },
+
+      // devOptions: {
+      //   enabled: true
+      // }
+    }),
+    react(),
+  ],
 })
