bdcd99aa8897a279671df2e98287d3e2ac81159d
Artur Perepelytsia <artur.perepelytsia.m@gmail.com>
2024-04-21 16:37:15 +0300

    @vite-pwa/assets-generator

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,46 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
+import {
+  minimal2023Preset,
+} from '@vite-pwa/assets-generator/config'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     VitePWA({
+      pwaAssets: {
+        preset: {
+          ...minimal2023Preset,
+          apple: {
+            ...minimal2023Preset.apple,
+            resizeOptions: {
+              background: '#00001A',
+            }
+          },
+          maskable: {
+            ...minimal2023Preset.maskable,
+            resizeOptions: {
+              background: '#00001A',
+            }
+          },
+          transparent: {
+            ...minimal2023Preset.transparent,
+            resizeOptions: {
+              background: '#00001A',
+            }
+          }
+
+        },
+        image: 'public/logo.png',
+      },
       registerType: 'autoUpdate',
       includeAssets: ['**/*'],
       manifest: {
-        'theme_color': '#f69435',
-        'background_color': '#f69435',
+        'theme_color': '#00001A',
+        'background_color': '#00001A',
         'scope': '/',
         'start_url': '/',
-        'icons': [
-          {
-            'src': '/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
-          },
-          {
-            'src': '/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
-          },
-          {
-            'src': '/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
-          {
-            'src': '/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
-          }
-        ]
       },
     }),
     react(),
